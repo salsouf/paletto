238cf90c6755d6a7dc87614793276d9b9a7e0bd3
boushine saloua <sboushin@prune>
2014-10-23 16:30:00 +0200

    hi

--- src/Engine.js
+++ src/Engine.js
@@ -1,6 +1,20 @@
 'use strict';
 
-var Engine = function () {
+var paletto = function () {
+
+    var board;
+    var Marblescolor;
+
+    var init=function(){
+
+        board = new Array(6);
+        for(i=0; i<6; i++){
+            board[i]=new Array(6);
+            for(j=0;j<6;j++){
+                board[i][j]= -1;
+            }
+        }
+    };
 
 // private attributes and methods
 
--- test/test.js
+++ test/test.js
@@ -3,4 +3,7 @@
 var PalettoTestCase = TestCase("PalettoTestCase");
 
 PalettoTestCase.prototype.testStory1 = function () {
+    var game = new paletto();
+
+    assertTrue (game.Marblescolor()==0);
 };
